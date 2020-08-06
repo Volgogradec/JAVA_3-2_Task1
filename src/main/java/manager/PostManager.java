8a96770caa710f6442dd3682ce04879931f3fc2a
Dmitry Evdokimov <vlg1evdokimov@gmail.com>
2020-08-06 23:03:58 +0300

    Task 2 - done

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,17 @@
+package manager;
+
+import ru.netology.Post;
+
+public class PostManager {
+    private Post[ ] post;
+
+    public Post[ ] search(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
+        // TODO: add logic
+        return null;
+    }
+
+    public void delete() {
+        // TODO: add logic
+    }
+
+}
